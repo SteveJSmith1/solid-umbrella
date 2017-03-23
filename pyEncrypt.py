f1d5a8efd5aa3002d8c5fbf462ad6635e00ac873
Steve Smith <stevejsmith567@gmail.com>
2017-03-23 15:05:46 +0000

    Major changes to pyEncrypt.py
    1: Filename is now encoded as bytes into the file to encode in.
    2: Encoded filename now begins 'H-'
    3: Filename for decoded file fetched from encoded file.
    4. Arguments passed for output filename removed from functions.

--- pyEncrypt.py
+++ pyEncrypt.py
@@ -37,7 +37,7 @@ def writeBytes(bytes_, file):
 
 
 
-def fileHide(file_to_enc, f_to_enc_in, output_file, filebytes=False):
+def fileHide(file_to_enc, f_to_enc_in, filebytes=False):
     """
     to hide a file:
     
@@ -63,29 +63,32 @@ def fileHide(file_to_enc, f_to_enc_in, output_file, filebytes=False):
         try a .wav, .mp4 instead of an .mp3 perhaps""")
         return
           
-        
-    
+    # encoding the filename into the file   
+    name_array = filenameToBytes(file_to_enc)
+    f_to_enc_in_b[2000:2350] = name_array
     
     # encoding file byte length info into   
     # file to encode in
     len_f_t_e_b = int(len(file_to_enc_b))
     len_f_as_bytes = len_f_t_e_b.to_bytes(10, byteorder='big')
     f_to_enc_in_b[1000:1010] = len_f_as_bytes
-    # encoding image information
+    # encoding file contents at the halfway point
     halfway = int(len(f_to_enc_in_b)/2)
     f_to_enc_in_b[halfway:halfway+len_f_t_e_b] = file_to_enc_b
     # writing output file
-    writeBytes(f_to_enc_in_b, output_file)
+    out_file_name = 'H-' + f_to_enc_in
+    writeBytes(f_to_enc_in_b, out_file_name)
     
     # returning bytes for equality check in
     # testFunction()
     if filebytes==True:
-        return file_to_enc_b
+        return file_to_enc_b, out_file_name
     else:
         return
 
 
-def fileFind(enc_file, out_file, filebytes=False):
+
+def fileFind(enc_file, filebytes=False):
     """
     to find a file hidden using fileHide()
 
@@ -101,12 +104,16 @@ def fileFind(enc_file, out_file, filebytes=False):
     # extracting encded
     # info about file length in bytes
     out_file_len = int.from_bytes(enc_bytes[1000:1010], byteorder='big')
+    # extracting encoded filename
+    name_array = enc_bytes[2000:2350]
+    # converting bytes to string of filename
+    filename = bytesToFilename(name_array)
     # extracting encoded out_file bytes
     halfway = int(len(enc_bytes)/2)
     
     out_file_bytes = enc_bytes[halfway:halfway + out_file_len]
     # write jpg
-    writeBytes(out_file_bytes, out_file)
+    writeBytes(out_file_bytes, filename)
     #returning bytes for equality test in 
     # testFunction()
     if filebytes==True:
@@ -114,19 +121,7 @@ def fileFind(enc_file, out_file, filebytes=False):
     else:
         return
 
-def testFunction(in_file, enc_file, out_enc, out_file):
-    """
-    This function performs a test operation
-    by first Hiding then Finding the file
-    to encode. Used to ascertain if the
-    output is usable
-    """
-    in_bytes = fileHide(in_file, enc_file, out_enc, filebytes=True)
-    out_bytes = fileFind(out_enc, out_file, filebytes=True)
-    
-    print(in_file, ' equals ', out_file, in_bytes==out_bytes)
-    return
- 
+
 #     3: write logical tests to ensure the length 
 #     of the image_bytes is less than the mp3_bytes
 
@@ -145,10 +140,13 @@ def fileCheck(in_file_bytes, enc_file_bytes):
 #     4: encode the filetype as bytes and insert
 #     into the audio file and append filetpye
 #     to output filename
- 
+
 def filenameToBytes(filename):
     # setting up a fairly large bytearray
-    # to contain the filename
+    # to contain the filename.
+    # if 350 is altered, the corresponding 
+    # slices in the main functions will
+    # need to be changed
     name_array = bytearray(350)
     # create a bytearray of the filename
     filename_bytes = bytearray(filename, 'utf-8')
@@ -163,6 +161,19 @@ def bytesToFilename(name_array):
     filename_bytes = name_array[0:name_array.find(0)]
     filename = filename_bytes.decode()
     return filename
+
+def testFunction(in_file, enc_file):
+    """
+    This function performs a test operation
+    by first Hiding then Finding the file
+    to encode. Used to ascertain if the
+    output is usable
+    """
+    in_bytes, out_file_name = fileHide(in_file, enc_file, filebytes=True)
+    out_bytes = fileFind(out_file_name, filebytes=True)
+    
+    print('Files are equal ', in_bytes==out_bytes)
+    return
 
 
 #     5: look at embedding file over a many 
