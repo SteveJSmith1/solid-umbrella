0df0467eabe9de27f2159a05652fb5ea63c30e9a
Steve Smith <stevejsmith567@gmail.com>
2017-03-23 15:37:38 +0000

    Major rewrite of the fileHide and fileFind functions
    Tidied up the code, created decodeBytes and encodeBytes to do the processing.

--- pyEncrypt.py
+++ pyEncrypt.py
@@ -62,22 +62,13 @@ def fileHide(file_to_enc, f_to_enc_in, filebytes=False):
     Suggest using a larger file;
         try a .wav, .mp4 instead of an .mp3 perhaps""")
         return
-          
-    # encoding the filename into the file   
-    name_array = filenameToBytes(file_to_enc)
-    f_to_enc_in_b[2000:2350] = name_array
     
-    # encoding file byte length info into   
-    # file to encode in
-    len_f_t_e_b = int(len(file_to_enc_b))
-    len_f_as_bytes = len_f_t_e_b.to_bytes(10, byteorder='big')
-    f_to_enc_in_b[1000:1010] = len_f_as_bytes
-    # encoding file contents at the halfway point
-    halfway = int(len(f_to_enc_in_b)/2)
-    f_to_enc_in_b[halfway:halfway+len_f_t_e_b] = file_to_enc_b
+    # processing the bytes
+    encoded_bytes = encodeBytes(file_to_enc, f_to_enc_in_b, file_to_enc_b)    
+    
     # writing output file
     out_file_name = 'H-' + f_to_enc_in
-    writeBytes(f_to_enc_in_b, out_file_name)
+    writeBytes(encoded_bytes, out_file_name)
     
     # returning bytes for equality check in
     # testFunction()
@@ -85,6 +76,26 @@ def fileHide(file_to_enc, f_to_enc_in, filebytes=False):
         return file_to_enc_b, out_file_name
     else:
         return
+
+def encodeBytes(file_to_enc, f_to_enc_in_b, file_to_enc_b):
+    # fetching the array containing the filename   
+    name_array = filenameToBytes(file_to_enc)
+    # overwriting bytes with filename
+    f_to_enc_in_b[2000:2350] = name_array
+    
+    
+    # getting the length of the bytes to hide
+    len_f_t_e_b = int(len(file_to_enc_b))
+    # turning this integer it a bytes object
+    # of length 10
+    len_f_as_bytes = len_f_t_e_b.to_bytes(10, byteorder='big')
+    # overwriting bytes with the bytes of length
+    f_to_enc_in_b[1000:1010] = len_f_as_bytes
+    # finding point in file to start 'hiding'
+    halfway = int(len(f_to_enc_in_b)/2)
+    # overwrite the bytes with the bytes to 'hide'
+    f_to_enc_in_b[halfway:halfway+len_f_t_e_b] = file_to_enc_b
+    return f_to_enc_in_b
 
 
 
@@ -101,6 +112,18 @@ def fileFind(enc_file, filebytes=False):
     """
     # fetching bytes from encoded file
     enc_bytes = readBytes(enc_file)
+    # fetching information from encoded bytes
+    out_file_bytes, filename = decodeBytes(enc_bytes)
+    # write file
+    writeBytes(out_file_bytes, filename)
+    #returning bytes for equality test in 
+    # testFunction()
+    if filebytes==True:
+        return out_file_bytes
+    else:
+        return
+
+def decodeBytes(enc_bytes):
     # extracting encded
     # info about file length in bytes
     out_file_len = int.from_bytes(enc_bytes[1000:1010], byteorder='big')
@@ -112,18 +135,10 @@ def fileFind(enc_file, filebytes=False):
     halfway = int(len(enc_bytes)/2)
     
     out_file_bytes = enc_bytes[halfway:halfway + out_file_len]
-    # write jpg
-    writeBytes(out_file_bytes, filename)
-    #returning bytes for equality test in 
-    # testFunction()
-    if filebytes==True:
-        return out_file_bytes
-    else:
-        return
+    
+    return out_file_bytes, filename
 
-
-#     3: write logical tests to ensure the length 
-#     of the image_bytes is less than the mp3_bytes
+  
 
 def fileCheck(in_file_bytes, enc_file_bytes):
     """
@@ -136,10 +151,7 @@ def fileCheck(in_file_bytes, enc_file_bytes):
     else:
         return False
 
-     
-#     4: encode the filetype as bytes and insert
-#     into the audio file and append filetpye
-#     to output filename
+
 
 def filenameToBytes(filename):
     # setting up a fairly large bytearray
@@ -162,6 +174,7 @@ def bytesToFilename(name_array):
     filename = filename_bytes.decode()
     return filename
 
+
 def testFunction(in_file, enc_file):
     """
     This function performs a test operation
@@ -175,15 +188,28 @@ def testFunction(in_file, enc_file):
     print('Files are equal ', in_bytes==out_bytes)
     return
 
+#==============================================
+# Scratchpad:
+  
+#   
+#   1b: Add F- to beginning of found filename
 
-#     5: look at embedding file over a many 
+#   2: Option to delete Original File
+#        when encoding
+
+#   2: 2b check if file already exists
+#       ask to overwrite if it does
+#       ask to specify filename if not
+
+#   3: look at embedding file over a many 
 #     different ranges
      
-#     6: look at using an encryption of the 
+#   4:  look at using an encryption of the 
 #     image_bytes before inserting into the 
 #     mp3_bytes
 
-#     7: Investigate why pdfs fail and 
+#   5:  Investigate why pdfs fail and 
 #        find an avenue to support them
+#        (it may just need a larger file)
      
 #===============================================           
