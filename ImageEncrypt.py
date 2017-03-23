7dd2ea0f1363dbf132e3e7efa079cf98c816f858
Steve Smith <stevejsmith567@gmail.com>
2017-03-23 11:55:14 +0000

    Removed individual tests and wrote test function

--- ImageEncrypt.py
+++ ImageEncrypt.py
@@ -45,136 +45,20 @@ def imageFind(mp3_file, output_file):
     writeBytes(im_bytes, output_file)
     return
 
-
- 
- 
-#=============================================
-
-## Testing
-
-#==============================================
-
-# test 1 - jpg and mp3 hide and find from resulting file
-
-def test_jpg_to_mp3_hide_and_back(im_file, mp3_file, mp3_out, im_out):
-    imageHide(im_file, mp3_file, mp3_out)
-    imageFind(mp3_out, im_out)
-    return
-
-            
-# Notes:
-    # There is an audible period of silence and
-    # missing "words" in the output file, this
-    # may be corrected by (5) in the to-do list
-
-im_file = '/home/steve/Pictures/20160616_163313.jpg'
-mp3_file = 'The Divine Comedy - To The Rescue..mp3'
-mp3_out = 'Test.mp3'
-im_out = 'Test.jpg'
-
-test_jpg_to_mp3_hide_and_back(im_file, mp3_file, mp3_out, im_out)
-
-test_jpg_to_mp3_hide_and_back('third.jpg', 'third.mp3', 'test.mp3', 'test.jpg')
-
-"""passes"""
-  
-
-
-#===============================================
-# scratchpad
-# to do:
-#     1: Test if this will work with wav files
-#     and rename mp3_bytes to audio_bytes if it does
-
-def test_jpg_to_wav_hide_and_back(im_file, wav_file, wav_out, im_out):
-    imageHide(im_file, wav_file, wav_out)
-    imageFind(wav_out, im_out)
-    return
-
-test_jpg_to_wav_hide_and_back('test.jpg', 'test.wav', 'out.wav', 'out.jpg')
-
-"""
-passes, puts "encoded noise" into the wav
-"""
-
- 
-#     2: Test if this will work with video files
-#     and rename mp3_bytes/audio_bytes to media 
-#     bytes if it does
-
-def test_jpg_to_video_hide_and_back
-test_jpg_to_video_hide_and_back('test.jpg', 'test.mp4', 'out.mp4', 'out.jpg')  
-
-"""
-passes
-"""
-
-#     2a: Test if this encodes text into audio
-
-     
-def test_txt_audio_and_back(in_file, enc_file, out_enc, out_file):
+def testFunction(in_file, enc_file, out_enc, out_file):
+    """
+    This function performs a test operation
+    by first Hiding then Finding the file
+    to encode. Used to ascertain if the
+    output is usable
+    """
     imageHide(in_file, enc_file, out_enc)
     imageFind(out_enc, out_file)
+    # add functionality to check if the
+    # found file is the same as the hidden file
     return
-
-test_txt_audio_and_back('test.txt', 'test.mp3', 'out.mp3', 'out.txt')
-"""
-passes
-"""
-
-#     2b: Test if this encodes any file 
-#       : (i.e. pdf, doc)
-
-def test_any_audio_and_back(in_file, enc_file, out_enc, out_file):
-    imageHide(in_file, enc_file, out_enc)
-    imageFind(out_enc, out_file)
-    return
-
-# test on doc to audio
-test_any_audio_and_back('test.doc', 'test.mp3', 'out.mp3', 'out.doc')
-
-"""
-pass
-"""
-
-# test on pdf to audio
-
-test_any_audio_and_back('test.pdf', 'test.mp3', 'out.mp3', 'out.pdf')
-
-"""
-fails
-
-# Breaks both output audio and output pdf
-# are the bytes encoded differently for a pdf?
-# pdf was ~= 1/2 the size of the audio file
-"""
-
-def test_any_video_and_back(in_file, enc_file, out_enc, out_file):
-    imageHide(in_file, enc_file, out_enc)
-    imageFind(out_enc, out_file)
-    return
-
-# test on .doc to video
-test_any_video_and_back('test.doc', 'test.mp4', 'out.mp4', 'out.doc')
-"""
-pass
-"""
-
-# test on pdf to video
-test_any_video_and_back('test.pdf', 'test.mp4', 'out.mp4', 'out.pdf')
-"""
-pass
-
-large pause then artefacts, but it was large pdf
-"""
-
-# test on mp3 to video
-test_any_video_and_back('test.mp3', 'test.mp4', 'out.mp4', 'out.mp3')
-"""
-pass
-
-large pause then artefacts, mp3 approx 0.1*mp4
-"""
+ 
+ 
 
 
 #     2c: try and break it by encoding a video
